18419497d124e9bca29c383c176856246d0f7ee3
#云淡然 <cloudcome@163.com>
2016-11-26 15:51:27 +0800

    ~ 不再兼容 IE8 及以下

--- src/index.js
+++ src/index.js
@@ -4,22 +4,23 @@ var compatible = require('blear.utils.compatible');
 
 var matchesSelector = compatible.js('matchesSelector', document.body);
 
-if (typeof CLASSICAL !== 'undefined' && CLASSICAL === true) {
-    exports.isMatched = function (el, selector) {
-        // http://tanalin.com/en/blog/2012/12/matches-selector-ie8/
-        var els = el.parentNode.querySelectorAll(selector),
-            count = els.length;
-
-        for (var i = 0; i < count; i++) {
-            if (els[i] === el) {
-                return true;
-            }
-        }
-
-        return false;
-    };
-} else {
-    exports.isMatched = function (el, selector) {
-        return el[matchesSelector](selector);
-    };
-}
+// if (typeof CLASSICAL !== 'undefined' && CLASSICAL === true) {
+//     exports.isMatched = function (el, selector) {
+//         // http://tanalin.com/en/blog/2012/12/matches-selector-ie8/
+//         var els = el.parentNode.querySelectorAll(selector),
+//             count = els.length;
+//
+//         for (var i = 0; i < count; i++) {
+//             if (els[i] === el) {
+//                 return true;
+//             }
+//         }
+//
+//         return false;
+//     };
+// } else {
+// iE9 支持
+exports.isMatched = function (el, selector) {
+    return el[matchesSelector](selector);
+};
+// }
