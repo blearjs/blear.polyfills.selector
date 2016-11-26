1b7ce2871eef81c1b1cb5440f5f228574c071649
#云淡然 <cloudcome@163.com>
2016-11-26 15:57:02 +0800

    ~ 合入 sibling

--- src/index.js
+++ src/index.js
@@ -24,3 +24,49 @@ exports.isMatched = function (el, selector) {
     return el[matchesSelector](selector);
 };
 // }
+
+
+
+if (typeof CLASSICAL !== 'undefined' && CLASSICAL === true) {
+    exports.prevElement = function (el) {
+        while (el = el.previousSibling) {
+            if (el.nodeType === 1) {
+                return el;
+            }
+        }
+
+        return null;
+    };
+    var nextElement = exports.nextElement = function (el) {
+        while (el = el.nextSibling) {
+            if (el.nodeType === 1) {
+                return el;
+            }
+        }
+
+        return null;
+    };
+    exports.firstElementChild = function (el) {
+        var firstChild = el.firstChild;
+
+        if (!firstChild) {
+            return firstChild;
+        }
+
+        if (firstChild.nodeType === 1) {
+            return firstChild;
+        }
+
+        return nextElement(firstChild);
+    };
+} else {
+    exports.prevElement = function (el) {
+        return el.previousElementSibling;
+    };
+    exports.nextElement = function (el) {
+        return el.nextElementSibling;
+    };
+    exports.firstElementChild = function (el) {
+        return el.firstElementChild;
+    };
+}
